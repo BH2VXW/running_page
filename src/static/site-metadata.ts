17b5beddbd3c6c9be40eecc7b1af39469a33314f
Hongbai <love2012@outlook.com>
2023-10-12 14:08:33 +0800

    Update site-metadata.ts
    修改站点信息

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,13 +11,13 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://running.shui.cm',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: '我的运动展示页',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://bigger.ee',
     },
     {
       name: 'About',
